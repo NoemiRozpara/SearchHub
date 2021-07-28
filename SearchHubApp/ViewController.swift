9a7a38ae318109e540f909e074378851888bbe73
Noemi Rozpara <nrozpara@gmail.com>
2021-07-28 16:33:07 +0200

    Initial Commit

---
+++ SearchHubApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchHubApp
+//
+//  Created by Noemi Rozpara on 28/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
