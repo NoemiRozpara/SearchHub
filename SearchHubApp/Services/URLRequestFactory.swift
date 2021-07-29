2198e890fe470fbf1f161f0b9a999d92d5fb8176
Noemi Rozpara <nrozpara@gmail.com>
2021-07-29 21:26:54 +0200

    fix: request factory bug

--- SearchHubApp/Services/URLRequestFactory.swift
+++ SearchHubApp/Services/URLRequestFactory.swift
@@ -13,17 +13,17 @@ enum URLRequestFactory {
             "Content-Type": "application/json",
             "cache-control": "no-cache",
             "accept": "application/vnd.github.v3+json",
-            // add user token etc
+//            // add user token etc
         ]
-        
+
         var request = URLRequest(
             url: url,
             cachePolicy: .useProtocolCachePolicy,
             timeoutInterval: 10.0
         )
-        request.httpMethod = "POST"
-        request.allHTTPHeaderFields = headers
         
+        request.allHTTPHeaderFields = headers
+
         return request
     }
     
